1fc2dd5e29a0c360b5a39de1a5d195bb46feff63
John Schellinger <john.schellinger@genesys.com>
2021-10-30 07:45:30 -0400

    Remove controller exposing stock entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClockGateway } from './clock/clock.gateway';
 import { ClockModule } from './clock/clock.module';
 import { ConfigModule } from '@nestjs/config';
 import { FeedModule } from './feed/feed.module';
 import { MarketModule } from './market/market.module';
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { RandomService } from './random/random.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
